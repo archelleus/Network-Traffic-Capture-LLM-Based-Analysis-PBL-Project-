7c18eabbe11895fe21768ea08393b5a13e5cc211
archelleus <133577546+archelleus@users.noreply.github.com>
2025-11-23 10:09:03 +0530

    Update and rename run_analysis.py to main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# main.py
 from network_detection import capture_network
 from LLM_summary import summarize_traffic
 
 def main():
-    interface = "Wi-Fi"  # change if needed
+    interface = "Wi-Fi"  
     csv_path = "traffic_log.csv"
     run_time = 10
 
-    # Step 1: Run capture for 30 seconds (printing live)
+    
     print("[*] Starting capture...")
     proto_counts, ip_counts = capture_network(interface=interface, run_time=run_time, csv_file_path=csv_path)
 
-    # Step 2: Generate summary and LLM analysis
+
     print("\n[*] Generating summary...\n")
     summarize_traffic(csv_path)
 
 if __name__ == "__main__":
     main()
+
